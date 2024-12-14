c44083ea4efc2e0e960d64a65656044cc15ff4c6
michael <mike.michaelmilstead@gmail.com>
2024-12-13 16:33:29 -0800

    remove non-starter files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,9 +25,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      {process.env.NEXT_PUBLIC_ACKE_DOMAIN_ID && (
-        <script async src={`${process.env.NEXT_PUBLIC_ACKE_SERVER}/tracker.js`} data-ackee-server={process.env.NEXT_PUBLIC_ACKE_SERVER} data-ackee-domain-id={process.env.NEXT_PUBLIC_ACKE_DOMAIN_ID}></script>
-      )}
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,10 @@
 "use client";
 import CRMDashboard from "@/components/dashboard/crm-dashboard";
-import TitleSection from "@/components/title-section";
-import hydra from "@/hydra-config";
-import ControlBar from "../components/control-bar";
 
 export default function Home() {
   return (
     <div className="min-h-screen p-8 pb-20 font-[family-name:var(--font-geist-sans)] bg-[#2E3645]">
       <main className="flex flex-col gap-8 items-center justify-center pt-24">
-        <TitleSection />
-        <div className="w-full max-w-7xl flex flex-col gap-4 justify-center items-center">
-          <ControlBar hydra={hydra} />
-        </div>
         <CRMDashboard />
       </main>
     </div>
